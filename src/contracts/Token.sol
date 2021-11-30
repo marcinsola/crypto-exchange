ffa3deb14dc24d4c160d3bbe6ba5b01ce1a3b4ca
Marcin Sola <marcinsolaonline@gmail.com>
2021-11-30 19:40:31 +0100

    Set token fields

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -2,5 +2,12 @@
 pragma solidity 0.8.10;
 
 contract Token {
-    string public name = "My Name";
+    string public name = "Solski Token";
+    string public symbol = "SLK";
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+    constructor() {
+        totalSupply = 1000000 * (10**18);
+    }
 }
